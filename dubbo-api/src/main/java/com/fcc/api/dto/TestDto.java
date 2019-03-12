77eb8baf5e2afd239fe79afe28b1b5eb69fdac9a
feicongcong <734820209@qq.com>
2019-03-13 00:34:44 +0800

    first commit

---
+++ dubbo-api/src/main/java/com/fcc/api/dto/TestDto.java
@@ -0,0 +1,23 @@
+package com.fcc.api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TestDto implements Serializable {
+    private String field1;
+    private String field2;
+    private String field3;
+    private String field4;
+    private String field5;
+    private String field6;
+    private String field7;
+    private String field8;
+    private String field9;
+    private String field10;
+}
